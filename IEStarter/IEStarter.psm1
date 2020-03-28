43dca7789784cab330d900052f2bb2cbd29596f1
isudzumi <isudzumi@gmail.com>
2020-03-29 02:22:36 +0900

    Add module

---
+++ IEStarter/IEStarter.psm1
@@ -0,0 +1,30 @@
+﻿<#
+    .Synopsis
+    Add custom headers for IE debugging
+
+    .Description
+    You can start up IE with adding or modifying custom HTTP headers interactively.
+    This module is for Windows only.
+
+    .Parameter Uri
+    Specify the website URL you're heading to.
+
+    .Parameter Headers
+    Custom headers you would like to add.
+
+    .Example
+    Debug-IE -Uri example.com
+
+#>
+
+function Debug-IE {
+param(
+    [string] $Uri,
+    [string[]] $Headers
+    )
+    $ie = New-Object -ComObject InternetExplorer.Application
+    $ie.Visible = $true
+    $ie.Navigate($Uri, $null, "_self", $null, $Headers -join "`r`n")
+}
+
+Export-ModuleMember -Function Debug-IE
