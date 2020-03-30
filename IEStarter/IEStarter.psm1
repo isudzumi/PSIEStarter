69b3c9e1879ce0dac2d35b617a5b61d5da9971f3
isudzumi <isudzumi@gmail.com>
2020-03-30 12:26:47 +0900

    Update module implementation

--- IEStarter/IEStarter.psm1
+++ IEStarter/IEStarter.psm1
@@ -1,4 +1,36 @@
-﻿<#
+﻿class IEOperation {
+
+    hidden $Ie
+    [string]hidden $Uri
+    [string[]]hidden $Headers
+
+    IEOperation([string]$uri, [string[]]$headers) {
+        $this.Uri = $uri
+        $this.Headers = $headers
+        $this.Ie = New-Object -ComObject InternetExplorer.Application
+        $this.Ie.Visible = $true
+        $this.Ie.Navigate($this.Uri, $null, "_self", $null, $this.Headers -join "`r`n")
+    }
+
+    [void] Reload() {
+        $this.Ie.Navigate($this.Uri, $null, "_self", $null, $this.Headers -join "`r`n")
+    }
+
+    [void] ListHeaders() {
+        $this.Headers
+    }
+}
+
+function Show-Message {
+    Write-Host 'Select your behavior'
+
+    Write-Host 'r: reload IE'
+    Write-Host 'e: edit custom headers'
+    Write-Host 'l: list selected custom headers'
+    Write-Host 'q: quit'
+}
+
+<#
     .Synopsis
     Add custom headers for IE debugging
 
@@ -22,9 +54,23 @@ param(
     [string] $Uri,
     [string[]] $Headers
     )
-    $ie = New-Object -ComObject InternetExplorer.Application
-    $ie.Visible = $true
-    $ie.Navigate($Uri, $null, "_self", $null, $Headers -join "`r`n")
+    $ie = [IEOperation]::new($Uri, $Headers)
+    do {
+        Show-Message
+        try {
+            [ValidatePattern("[relq]")]$instruction = Read-Host 'Please input'
+            switch -Exact ($instruction) {
+                "r" {
+                    $ie.Reload
+                }
+                "l" {
+                    $ie.ListHeaders
+                }
+            }
+        }
+        catch {}
+    }
+    until ($instruction -eq 'q')
 }
 
 Export-ModuleMember -Function Debug-IE
